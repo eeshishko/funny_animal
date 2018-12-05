6f0d06a099b478178b868e20f56e30e32f72fdee
EvgenyShishko <alchemist.sc2@gmail.com>
2018-12-05 10:57:03 +0300

    Changed game over controller

--- HitTheTree/HitTheTree/View Controllers/GameOverViewController.swift
+++ HitTheTree/HitTheTree/View Controllers/GameOverViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class GameOverViewController: UIViewController {
+	@IBOutlet weak var scoreLabel: UILabel!
+	@IBOutlet weak var recordLabel: UILabel!
+	
+	var menuClickHandler: (() -> (Void))?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		let score = Int(scoreLabel.text!)!
+		let records: [GameResult]
+		recordLabel.isHidden = true
+		if let encodedData = UserDefaults.standard.object(forKey: UserDefaultKeys.records) as? Data {
+			records = NSKeyedUnarchiver.unarchiveObject(with: encodedData) as! [GameResult]
+			for rec in records {
+				if score > rec.score {
+					recordLabel.isHidden = false
+					break
+				}
+			}
+		}
 	}
-	
+	@IBAction func callMenuBlock(_ sender: Any) {
+		menuClickHandler?()
+	}
 }
