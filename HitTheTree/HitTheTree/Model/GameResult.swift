e70b2743b176fd8db1f996b4a15178da22c47fb0
EvgenyShishko <alchemist.sc2@gmail.com>
2018-12-06 22:39:40 +0300

    Added player name recording

--- HitTheTree/HitTheTree/Model/GameResult.swift
+++ HitTheTree/HitTheTree/Model/GameResult.swift
@@ -11,20 +11,24 @@ import Foundation
 class GameResult: NSObject, NSCoding {
 	let score: Int
 	let date: Date
+	let playerName: String
 	
-	init(score: Int, date: Date) {
+	init(score: Int, date: Date, playerName: String) {
 		self.score = score
 		self.date = date
+		self.playerName = playerName
 	}
 	
 	func encode(with aCoder: NSCoder) {
 		aCoder.encode(score, forKey: "score")
 		aCoder.encode(date, forKey: "date")
+		aCoder.encode(playerName, forKey: "playerName")
 	}
 	
 	required convenience init?(coder aDecoder: NSCoder) {
 		let score = aDecoder.decodeInteger(forKey: "score")
 		let date = aDecoder.decodeObject(forKey: "date") as! Date
-		self.init(score: score, date: date)
+		let playerName = aDecoder.decodeObject(forKey: "playerName") as! String
+		self.init(score: score, date: date, playerName: playerName)
 	}
 }
