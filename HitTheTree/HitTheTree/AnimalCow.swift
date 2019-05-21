eac81bf0310ed2e76f6f91031ac234d2fcd56687
Matvey Kravtsov <kravtsovguy@gmail.com>
2019-05-21 22:27:13 +0300

    no message

--- HitTheTree/HitTheTree/AnimalCow.swift
+++ HitTheTree/HitTheTree/AnimalCow.swift
@@ -12,7 +12,7 @@ extension Animal {
     
     func cowMaterials() {
         let material1 = SCNMaterial()
-        material1.diffuse.contents = UIImage(named: "art.scnassets/cow/11_01.jpg")
+        material1.diffuse.contents = UIImage(named: "art.scnassets/cow/11_01.png")
         material1.locksAmbientWithDiffuse = true
         
         let material1Dead = SCNMaterial()
